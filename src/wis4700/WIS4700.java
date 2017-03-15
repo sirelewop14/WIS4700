70a71ebd6a4e95e3e750ba974b755be019807686
rhys <rhys@m-002500ee2073.wright.edu>
2017-03-15 16:32:11 -0400

    Started working on stemming and stop word removal.

--- src/wis4700/WIS4700.java
+++ src/wis4700/WIS4700.java
@@ -5,6 +5,12 @@
  */
 package wis4700;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import wis4700.jgibblda.Inferencer;
 import wis4700.jgibblda.LDACmdOption;
 import wis4700.jgibblda.Model;
@@ -16,8 +22,9 @@ public class WIS4700 {
 
     /**
      * @param args the command line arguments
+     * @throws java.io.FileNotFoundException
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException, IOException {
         LDACmdOption ldaOption = new LDACmdOption(); 
         ldaOption.est = true; 
         //ldaOption.inf = true;
@@ -30,12 +37,36 @@ public class WIS4700 {
         ldaOption.niters = 10;
         ldaOption.dfile = "/../user_tweets_fpl_from_twitter.csv";
         
-        Model newModel = new Model();
-        newModel.initNewModel(ldaOption);
+        FileReader fin = new FileReader("/Users/rhys/LDA_Test/sample_data.csv");
+        FileWriter fout = new FileWriter("/Users/rhys/LDA_Test/sample_data_stopped.txt");
+        BufferedReader in = new BufferedReader(fin);
+        BufferedWriter out = new BufferedWriter(fout);
+        while (in.ready()){
+            String line = in.readLine();
+            System.out.println(line);
+            String stemmed = Stopwords.stemString(line);
+            String stopped = Stopwords.removeStemmedStopWords(stemmed);
+            out.write(stopped+"\n", 0, stopped.length());
+            System.out.println(stopped);
+        }
+        out.flush();
+        out.close();
+        in.close();
         
-        newModel.saveModel(ldaOption.modelName);
         
-        Inferencer inferencer = new Inferencer(); 
+        
+        
+       // Model newModel = new Model();
+       // newModel.initNewModel(ldaOption);
+        
+        
+        
+        
+        //newModel.saveModel(ldaOption.modelName);
+        
+        //Inferencer inferencer = new Inferencer(); 
+        
+        
         
         
        //Model newModel2 = inferencer.inference();
