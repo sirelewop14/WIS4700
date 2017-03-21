4e12e28cf8210c06eeb850336ea23d31a9fb8f9a
rhys <rhys@m-002500ee2073.wright.edu>
2017-03-21 12:57:01 -0400

    Finally was able to get the syntax correct for generating a model from scratch. Testing with large data sets now.

--- src/wis4700/WIS4700.java
+++ src/wis4700/WIS4700.java
@@ -5,7 +5,9 @@
  */
 package wis4700;
 
+import java.io.BufferedWriter;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.net.InetAddress;
 import org.elasticsearch.action.search.SearchResponse;
@@ -15,11 +17,8 @@ import org.elasticsearch.common.transport.InetSocketTransportAddress;
 import org.elasticsearch.search.SearchHit;
 import org.elasticsearch.search.SearchHits;
 import org.elasticsearch.transport.client.PreBuiltTransportClient;
-
-
-import wis4700.jgibblda.Inferencer;
+import wis4700.jgibblda.Estimator;
 import wis4700.jgibblda.LDACmdOption;
-import wis4700.jgibblda.Model;
 
 /**
  *
@@ -32,149 +31,63 @@ public class WIS4700 {
      * @throws java.io.FileNotFoundException
      */
     public static void main(String[] args) throws FileNotFoundException, IOException {
-//        LDACmdOption ldaOption = new LDACmdOption(); 
-//        ldaOption.est = true; 
-//        //ldaOption.inf = true;
-//        //ldaOption.alpha = (50 / ldaOption.K);
-//        //ldaOption.beta = 0.1;
-//        ldaOption.savestep = 100;
-//        ldaOption.twords = 20;
-//        ldaOption.dir = "/Users/rhys/LDA_Test/Model"; 
-//        ldaOption.modelName = "newdocs"; 
-//        ldaOption.niters = 10;
-//        ldaOption.dfile = "/../user_tweets_fpl_from_twitter.csv";
-//        
-//        FileReader fin = new FileReader("/Users/rhys/LDA_Test/sample_data.csv");
-//        FileWriter fout = new FileWriter("/Users/rhys/LDA_Test/sample_data_stopped.txt");
-//        BufferedReader in = new BufferedReader(fin);
-//        BufferedWriter out = new BufferedWriter(fout);
-//        while (in.ready()){
-//            String line = in.readLine();
-//            System.out.println(line);
-//            String stemmed = Stopwords.stemString(line);
-//            String stopped = Stopwords.removeStemmedStopWords(stemmed);
-//            out.write(stopped, 0, stopped.length());
-//            out.newLine();
-//            System.out.println(stopped);
-//        }
-//        out.flush();
-//        out.close();
-//        in.close();
-
-//        
+        //ES Database Connection
         Settings settings = Settings.builder().put("cluster.name", "elasticsearch_rhys").build();
         TransportClient client = new PreBuiltTransportClient(settings).addTransportAddress(new InetSocketTransportAddress(InetAddress.getByName("127.0.0.1"), 9300));
-        
-//        SearchResponse response = client.prepareSearch("epl")
-//                .setTypes("elps")
-//                .setSearchType(SearchType.DFS_QUERY_THEN_FETCH)
-//                .setQuery(QueryBuilders.termQuery("multi", "test"))
-//                .setPostFilter(QueryBuilders.rangeQuery("age").from(12).to(18))
-//                .setFrom(0).setSize(60).setExplain(true)
-//                .get();
-        
-        SearchResponse resp1 = client.prepareSearch("epl").get();
+        //Call for data from ES
+        SearchResponse resp1 = client.prepareSearch("epl").setFrom(0).setSize(80000).get();
         SearchHits hits = resp1.getHits();
         SearchHit[] hitArray = hits.getHits();
-        String[] messages = new String[hitArray.length];
         
+        //Parse data from ES and write out to file
+        FileWriter fout = new FileWriter("/Users/rhys/LDA_Test/sample_data_stopped.txt");
+        BufferedWriter out = new BufferedWriter(fout);
+        String size = Integer.toString(hitArray.length);
+        out.write(size, 0, size.length());
+        out.newLine();
         for (int i = 0; i < hitArray.length; i++) {
-            //Map<String, Object> json = hitArray[i].getSource();
-            //InternalSearchHit ishit =  hitArray1.getSource();
-            //System.out.println(json.size());
-            //System.out.println();
-            //System.out.println(hitArray[i].getSource().get("Username"));
-            //System.out.println(hitArray[i].getSource().get("Message"));
-            messages[i] = hitArray[i].getSource().get("Message").toString();
-            
-            //System.out.println(hitArray1);
-            //System.out.println(hitArray1.getInnerHits());
+            String line = "";
+            try{
+                line = hitArray[i].getSource().get("Message").toString();
+            } catch (Exception e){
+                System.out.println(e);
+                System.out.println("At Index: "+i);
+            }
+            //Stem and remove stop words
+            String stemmed = Stopwords.stemString(line);
+            String stopped = Stopwords.removeStemmedStopWords(stemmed);
+            out.write(stopped, 0, stopped.length());
+            out.newLine();
         }
-//        for(int j = 0; j < messages.length; j++){
-//            System.out.println(messages[j]);
-//        }
-        
-        
+        out.flush();
+        out.close();
+        //LDA Create new model with settings
         LDACmdOption ldaOption = new LDACmdOption(); 
         ldaOption.est = true; 
         //ldaOption.inf = true;
-        ldaOption.alpha = (50 / ldaOption.K);
-        ldaOption.beta = 0.1;
-        ldaOption.savestep = 100;
-        ldaOption.twords = 20;
+        //ldaOption.alpha = (50 / ldaOption.K);
+        //ldaOption.beta = 0.1;
+        ldaOption.savestep = 500;
+        ldaOption.twords = 10;
         ldaOption.dir = "/Users/rhys/LDA_Test/Model"; 
         ldaOption.modelName = "newdocs"; 
-        ldaOption.niters = 10;
+        //ldaOption.niters = 10;
+        ldaOption.dfile = "../sample_data_stopped.txt";
         
-        //ldaOption.dfile = "/../user_tweets_fpl_from_twitter.csv";
+        Estimator estimator = new Estimator();
+        estimator.init(ldaOption);
         
-        Model newModel = new Model();
-        newModel.initNewModel(ldaOption);
+        estimator.estimate();
         
-        newModel.saveModel(ldaOption.modelName);
+        //Model newModel = new Model();
+        //newModel.initNewModel(ldaOption);
+        
+        //newModel.saveModel(ldaOption.modelName);
         
         
         
-        Inferencer inferencer = new Inferencer(); 
+        //Inferencer inferencer = new Inferencer(); 
         //inferencer.init(ldaOption);
         //Model newModel = inferencer.inference(messages);
-//        
-        // The input stream from the JSON response
-//        BufferedInputStream buffer = null;
-//
-//        // URL objects
-//        String url = "";
-//        URL urlObject = null;
-//        URLConnection con = null;
-//        String response = "";
-//
-//        // JSON objects
-//        JSONArray hitsArray = null;
-//        JSONObject hits = null;
-//        JSONObject source = null;
-//        JSONObject json = null;
-//
-//        try {
-//            // get a JSON object from ElasticSearch
-//            url = "http://localhost:9200/epl/_search";
-//
-//            // configure the URL request
-//            urlObject = new URL(url);
-//            con = urlObject.openConnection();
-//            con.setRequestProperty("User-Agent", "Mozilla/5.0");
-//
-//            buffer = new BufferedInputStream(con.getInputStream());
-//
-//            while (buffer.available() > 0) {
-//                response += (char) buffer.read();
-//            }
-//
-//            buffer.close();
-//
-//            // parse the JSON response 		
-//            json = new JSONObject(response);
-//            hits = json.getJSONObject("hits");
-//            hitsArray = hits.getJSONArray("hits");
-//            
-//            int size = hitsArray.length();
-//            
-//            String[] username = new String[size];
-//            String[] message = new String[size];
-//            for (int i = 0; i < hitsArray.length(); i++) {
-//                JSONObject h = hitsArray.getJSONObject(i);
-//                source = h.getJSONObject("_source");
-//                //string object = (source.getString("the string you want to get"));
-//                //System.out.println(source);
-//                username[i] = source.getString("Username");
-//                message[i] = source.getString("Message");
-//                
-//                System.out.println(username[i]);
-//                System.out.println(message[i]);
-//                
-//            }
-//        } catch (Exception e) {
-//            // handle the exception
-//        }
-
     }
 }
