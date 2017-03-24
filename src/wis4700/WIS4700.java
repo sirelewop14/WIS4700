fc0f8f5dd33a1e36e82a073716180e736248d7c6
rhys <rhys@m-002500ee2073.wright.edu>
2017-03-24 12:56:51 -0400

    Got some logic which reads from csv directly, then parses out the URL's and stop words, stemms the message and dumps them into a txt file for modeling.

--- src/wis4700/WIS4700.java
+++ src/wis4700/WIS4700.java
@@ -11,21 +11,11 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.net.InetAddress;
 import java.util.ArrayList;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-import org.elasticsearch.action.search.SearchResponse;
-import org.elasticsearch.client.transport.TransportClient;
-import org.elasticsearch.common.settings.Settings;
-import org.elasticsearch.common.transport.InetSocketTransportAddress;
-import org.elasticsearch.common.unit.TimeValue;
-import org.elasticsearch.search.SearchHit;
-import org.elasticsearch.transport.client.PreBuiltTransportClient;
 import wis4700.jgibblda.Estimator;
-import wis4700.jgibblda.Inferencer;
 import wis4700.jgibblda.LDACmdOption;
-import wis4700.jgibblda.Model;
 
 /**
  *
@@ -38,58 +28,6 @@ public class WIS4700 {
      * @throws java.io.FileNotFoundException
      */
     public static void main(String[] args) throws FileNotFoundException, IOException {
-        //ES Database Connection
-        /*
-        Settings settings = Settings.builder().put("cluster.name", "elasticsearch_rhys").build();
-        TransportClient client = new PreBuiltTransportClient(settings).addTransportAddress(new InetSocketTransportAddress(InetAddress.getByName("127.0.0.1"), 9300));
-        //Call for data from ES
-        //Create array for storing results
-        ArrayList<String> lines = new ArrayList<>();
-        SearchResponse scrollResp = client.prepareSearch("epl")
-                .setScroll(new TimeValue(60000))
-                .setSize(1000).get(); //max of 1000 hits will be returned for each scroll
-        //Scroll until no hits are returned
-        do {
-            for (SearchHit hit : scrollResp.getHits().getHits()) {
-                //Handle the hit...
-                String line = "";
-                try {
-                    line = hit.getSource().get("Message").toString();
-                } catch (Exception e) {
-                    System.out.println(e);
-                    System.out.println("At Index: " + hit.getId());
-                }
-                //Stem and remove stop words
-                String stemmed = Stopwords.stemString(line);
-                String stopped = Stopwords.removeStemmedStopWords(stemmed);
-                lines.add(stopped);
-            }
-            //Scroll again
-            scrollResp = client.prepareSearchScroll(scrollResp.getScrollId()).setScroll(new TimeValue(60000)).execute().actionGet();
-        } while (scrollResp.getHits().getHits().length != 0); // Zero hits mark the end of the scroll and the while loop.
-
-        //Parse data from ES and write out to file
-        FileWriter fout = new FileWriter("/Users/rhys/LDA_Test/sample_data_stopped.txt");
-        try (BufferedWriter out = new BufferedWriter(fout)) {
-            String size = Integer.toString(lines.size());
-            out.write(size, 0, size.length());
-            out.newLine();
-            //Print number of documents
-            System.out.println(lines.size());
-            for (int i = 0; i < lines.size(); i++) {
-                out.write(lines.get(i), 0, lines.get(i).length());
-                out.newLine();
-            }
-            out.flush();
-        }
-
-
-        
-        
-        //Empty array now that data is in text doc
-        lines.clear();
-        lines.trimToSize();
-         */
         ArrayList<String> messages = new ArrayList<>();
         String splitVal = ",";
         String urlPattern = "((https?|ftp|gopher|telnet|file|Unsure|http):((//)|(\\\\))+[\\w\\d:#@%/;$()~_?\\+-=\\\\\\.&]*)";
@@ -99,26 +37,25 @@ public class WIS4700 {
         int badCount = 0;
         try (BufferedReader bread = new BufferedReader(fread)) {
             while ((line = bread.readLine()) != null) {
-                //System.out.println(line);
                 String[] splitLine = line.split(splitVal);
-                //System.out.println(splitLine[0]);
-                //System.out.println(splitLine[1]);
                 if (splitLine.length <= 1) {
                     System.out.println("Bad Line!");
                     badCount++;
                 } else {
-                    //String urlFree = removeUrl(splitLine[1]);
                     Matcher m = p.matcher(splitLine[1]);
                     int i = 0;
-                    String urlFree = "";
-                    while (m.find()) {
-                        urlFree = splitLine[1].replaceAll(m.group(i), "").trim();
-                        i++;
+                    String urlFree = "";     
+                    if (m.find()) {
+                        m.reset();
+                        while (m.find()) {
+                            urlFree = splitLine[1].replaceAll(m.group(i), "").trim();
+                            i++;
+                        }
+                    } else {
+                        urlFree = splitLine[1];
                     }
-
                     String stemmed = Stopwords.stemString(urlFree);
                     String stopped = Stopwords.removeStemmedStopWords(stemmed);
-
                     messages.add(stopped);
                 }
 
@@ -129,7 +66,7 @@ public class WIS4700 {
             System.out.println(e);
         }
 
-        System.out.println(badCount);
+        System.out.println("Total bad lines: " + badCount);
 
         FileWriter fout = new FileWriter("/Users/rhys/LDA_Test/sample_data_stopped.txt");
         try (BufferedWriter out = new BufferedWriter(fout)) {
@@ -137,7 +74,7 @@ public class WIS4700 {
             out.write(size, 0, size.length());
             out.newLine();
             //Print number of documents
-            System.out.println(messages.size());
+            System.out.println("Total number of good lines: " + messages.size());
             for (int i = 0; i < messages.size(); i++) {
                 out.write(messages.get(i), 0, messages.get(i).length());
                 out.newLine();
@@ -175,9 +112,9 @@ public class WIS4700 {
 //                } 
         //This estimator creates a new model, but currently a model takes 4 hours
         //to build. This is commented out to avoid issues with overwriting.
-//        Estimator estimator = new Estimator();
-//        estimator.init(ldaOption);
-//        estimator.estimate();
+        Estimator estimator = new Estimator();
+        estimator.init(ldaOption);
+        estimator.estimate();
         //Model newModel = new Model();
         //newModel.initNewModel(ldaOption);
         //newModel.saveModel(ldaOption.modelName);
@@ -185,11 +122,4 @@ public class WIS4700 {
         //inferencer.init(ldaOption);
         //Model newModel = inferencer.inference(messages);
     }
-
-    public static String removeUrl(String commentstr) {
-
-        return commentstr;
-
-    }
-
 }
