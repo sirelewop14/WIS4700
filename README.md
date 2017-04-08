ff277b44c3d9f34ed9116127bf04d548bb57a189
sirelewop14 <rhys@jeeves.rhyspowell.info>
2017-04-08 16:49:12 -0400

    Wrote out all the documentation. Added some detail to the readme

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-#WIS4700
+This class provides several methods for utilizing LDA Analysis for topic 
+identification. The purpose of these functions is to allow a user to input
+a CSV file containing users and tweets from Twitter. The functions provided
+here are then used to parse the tweets, stem and remove stop words, and 
+then estimate an LDA model based of the data.
+
+Once the model has been created, an inferece can be performed on the CSV 
+data. When inferece has been performed, the user can then run the evaluation
+step. This step analyzes the raw data and calculates the weight of identified
+topics for individual users.
+
+Finally, once the weight of each topic per user has been calculated, the 
+labeling function can provide a report in CSV format w/ vectors for the 
+selected topics and labels. The final function SHOULD only be run once the 
+generated tword file (From inferecing step) has been read through, and topics
+of interest have been identified and labeled.
--- src/wis4700/WIS4700.java
+++ src/wis4700/WIS4700.java
@@ -8,12 +8,24 @@ import java.util.concurrent.TimeUnit;
 import wis4700.jgibblda.LDACmdOption;
 
 /**
- *
+ * This class provides a simple command line interface for working with the 
+ * TwitterLDA class. A user can select option and perform specific steps through
+ * the simple menu options. There are a few options which collect parameters from
+ * the user and then performs all the needed modeling, inferencing and evaluation
+ * steps automatically. I have also included a demo option which runs with pre-
+ * defined parameters to process a small file included in the repository for
+ * sample runs.
+ * 
  * @author rhys
  */
 public class WIS4700 {
 
     /**
+     * The main method. This function runs the simple interface for users to 
+     * interact with the TwitterLDA class. There are several options available
+     * to perform the individual steps of the modeling and analysis process,
+     * as well there is a demo option for out-of-the-box operation.
+     * 
      * @param args the command line arguments
      * @throws java.io.FileNotFoundException
      */
@@ -22,7 +34,7 @@ public class WIS4700 {
         boolean run = true;
 
         System.out.println("Please enter the parameters for modeling:");
-        System.out.println("All files are relative to the LDA Model Directory!");
+        System.out.println("All files should have absolute paths!");
         System.out.println("\tLDA Model Directory: ");
         String LDADirectory = scanner.nextLine().trim();
 
@@ -43,8 +55,8 @@ public class WIS4700 {
             System.out.println("3: Estimate new Model");
             System.out.println("4: Inference New Data");
             System.out.println("5: Evaluate Users");
-            System.out.println("6: Run 1,2,3,5 & Exit");
-            System.out.println("7: Label Topics");
+            System.out.println("6: Run 2,3,4,5 & Exit");
+            System.out.println("7: Label Topics - *Requires code modification*");
             System.out.println("8: Run Demo with defaults");
             System.out.println();
             try {
@@ -58,8 +70,7 @@ public class WIS4700 {
                         System.out.println("\tRaw CSV Input File: ");
                         rawInputFile = scanner.nextLine().trim();
                         System.out.println("\tProcessed CSV File Name: ");
-                        dataFileName = scanner.nextLine().trim();
-                        dataFile = LDADirectory + dataFileName;
+                        dataFile = scanner.nextLine().trim();
                         ArrayList<String> data = analyzer.readCSV(rawInputFile);
                         analyzer.saveMessages(data, dataFile);
                         break;
@@ -100,13 +111,12 @@ public class WIS4700 {
                         System.out.println("\tPlease enter the filename for T-Word Hit Output:");
                         twordHitOutput = scanner.nextLine().trim();
                         System.out.println("\tProcessed CSV File Name: ");
-                        dataFileName = scanner.nextLine().trim();
-                        dataFile = LDADirectory + dataFileName;
+                        dataFile = scanner.nextLine().trim();
                         ArrayList<String> data = analyzer.readCSV(rawInputFile);
                         analyzer.saveMessages(data, dataFile);
                         //Pause for flush to disk
                         TimeUnit.SECONDS.sleep(5);
-                        LDACmdOption options = analyzer.setLDAOptions(LDADirectory, dataFileName);
+                        LDACmdOption options = analyzer.setLDAOptions(LDADirectory, dataFile);
                         analyzer.performEstimation(options);
                         analyzer.performInference(options);
                         analyzer.evaluateUsers(twordsFile, rawInputFile, userOutput, twordHitOutput);
